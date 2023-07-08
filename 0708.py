f1f8a3d70d5acb6540a8318adbbb81b3e1ecfebb
JudasWu <134752681+JudasWu@users.noreply.github.com>
2023-07-08 07:52:51 +0000

    0708

--- 0708.py
+++ 0708.py
@@ -1,17 +1,20 @@
 import random
-min = 0
-max = 100
-bomb = random.randint(min,max)
-
-value = -1
-while value != bomb:
-    value = int(input(f'請猜{min}~{max}的數字:'))
+value1 = 'y'
+while value1 != 'n':
+    min = 0
+    max = 100
+    bomb = random.randint(min,max)
+    print({bomb})
+    value = -1
+    while value != bomb:
+        value = int(input(f'請猜{min}~{max}的數字:'))
     
-    if value < min or value > max:
-        continue
-    elif value > bomb :
-        max=value
-    else:
-        min = value
-print("爆炸了!!!")
+        if value < min or value > max:
+            continue
+        elif value > bomb :
+            max=value
+        else:
+            min = value
+    print("爆炸了!!!")
+    value1 = input('要不要繼續?(y/n)')
         
