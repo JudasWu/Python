d261cc570d3517b7f04883cf9a3d3f353822dd6b
JudasWu <134752681+JudasWu@users.noreply.github.com>
2023-08-19 07:13:35 +0000

    0819

--- 0819/main.py
+++ 0819/main.py
@@ -1,5 +1,8 @@
 import csv
 import pandas as pd
+import streamlit as st
+import ffn
+
 def getStockNames() -> pd.Series:
     with open('codeSearch.csv',encoding='utf-8',newline='') as file:
         next(file) #跳過第一行
@@ -11,5 +14,25 @@ def getStockNames() -> pd.Series:
     code_series:pd.Series = pd.Series(stock_codes)
     return code_series
 
+def displayData(dataFrame:pd.DataFrame,start_year) -> None:
+    st.subheader(f'{start_year}~目前的歷史資料')
+    st.dataframe(dataFrame)
+
+
+
 stockNames:pd.Series = getStockNames()
 print(stockNames)
+#多重選取
+stockNames_id = stockNames.index + '_' + stockNames.values
+options = st.sidebar.multiselect('請選擇',
+                       stockNames_id,
+                       placeholder='股票:')
+
+names:list[str] = []
+for name in options:
+    name_string = name.split("_")[0]
+    names.append(name_string + ".TW")
+#print(names)
+prices = ffn.get(names, start='2010-01-01')
+
+displayData(prices,start_year='2010-01-01')
